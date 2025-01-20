9e02e22d9d06446c4b57cb1acf90c03ee1dc7cb4
Taty <taty.bystrova@gmail.com>
2025-01-20 10:18:09 +0200

    add tasc-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 function getElementWidth(content, padding, border) {
-    
-    return content + (2 * padding) + (2 * border);
+    content = Number.parseInt(content);
+    padding = Number.parseInt(padding);
+    border = Number.parseInt(border);
+    return getElementWidth = Number.parseFloat(content) + (Number.parseFloat(padding) * 2) + (Number.parseFloat(border) * 2);
+   
 };
-
 
 getElementWidth("50px", "8px", "4px");
 getElementWidth("60px", "12px", "8.5px");
