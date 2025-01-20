4138a1cd49dbd369fbae5ef393988ec91c444211
Taty <taty.bystrova@gmail.com>
2025-01-20 17:54:07 +0200

    change return and concol.log in all tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
-    return makeTransaction = `You ordered ${quantity} droids worth ${pricePerDroid * quantity} credits!`;
+    return `You ordered ${quantity} droids worth ${pricePerDroid * quantity} credits!`;
 };
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,10 @@ function getElementWidth(content, padding, border) {
     content = Number.parseInt(content);
     padding = Number.parseInt(padding);
     border = Number.parseInt(border);
-    return getElementWidth =  content + (padding * 2) + (border * 2);
+    return content +  (padding * 2) + (border * 2);
    
 };
 
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
